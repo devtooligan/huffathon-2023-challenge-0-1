095b0921e10093dfffbe3a118f5b8cef3ea51f41
devtooligan <devtooligan@tooligan.dev>
2023-09-28 12:46:21 -0700

    fix

--- src/utils/CREATOR.sol
+++ src/utils/CREATOR.sol
@@ -16,7 +16,7 @@ library CREATOR {
     // IMPORTANT: CREATOR TO UPDATE THIS!
     // This function returns true or false if the solution is correct.
     // Write logic that will check if the provided solution is correct
-    function verify(address solution) public returns (bool) {
+    function verify(address solution) internal returns (bool) {
         // add logic to verify solution
         uint response = IChallenge(solution).stackIt();
         return response == 0x420;
@@ -25,14 +25,14 @@ library CREATOR {
     // IMPORTANT: CREATOR TO UPDATE THIS!
     // his function should return the number that should reported for gas for the solution.
     // It could be the measurement of a single function call or multiple.
-    function gasReport(address solution)  public returns (uint256 gasUsed) {
+    function gasReport(address solution)  internal returns (uint256 gasUsed) {
         // add logic to report gas cost of the relevant call or calls to the solution
         uint start = gasleft();
         IChallenge(solution).stackIt();
         gasUsed = start - gasleft();
     }
 
-    function challengeId() public pure returns (uint8) {
+    function challengeId() internal pure returns (uint8) {
         require(_challengeId != 0xff, "IMPORTANT: CREATOR to update challengeId!");
         return _challengeId;
     }
